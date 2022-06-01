d117656d0f9cce911d1a72257e5090c9ff6c5007
satoqz <satoqz@pm.me>
2022-06-01 21:12:56 +0200

    clean up

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # `discord-avatar`
+
+Use `https://discordav.deno.dev/{user_id}` for a redirect to the associated avatar.
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,9 @@ const manager = createRestManager({ token: Deno.env.get("TOKEN")! });
 const notFound = new Response("", { status: 404 });
 
 serve(async (req) => {
-  const { pathname } = new URL(req.url);
-  const id = pathname.replace(/.jpg|.jpeg|.png|.webp|\//g, "");
-  if (!id || id == "favicon.ico") return notFound.clone();
+  const matches = new URL(req.url).pathname.match(/^\/(\d{18})$/);
+  if (!matches) return notFound.clone();
+  const [_, id] = matches;
   try {
     const { avatar }: { avatar: string } = await manager.runMethod(
       manager,
