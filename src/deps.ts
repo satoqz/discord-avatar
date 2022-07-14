d426393b1c57cc9219b024de03c71f461844db0e
satoqz <satoqz@pm.me>
2022-07-14 12:17:20 +0200

    handle default avatars

--- src/deps.ts
+++ src/deps.ts
@@ -1,2 +1,2 @@
-export * from "https://deno.land/std@0.141.0/http/server.ts";
+export * from "https://deno.land/std@0.148.0/http/server.ts";
 export * from "https://deno.land/x/discordeno@13.0.0-rc45/rest/mod.ts";
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,32 @@
 import { serve, createRestManager } from "./deps.ts";
 
 const manager = createRestManager({ token: Deno.env.get("TOKEN")! });
-const notFound = new Response("", { status: 404 });
 
 serve(async (req) => {
   const matches = new URL(req.url).pathname.match(/^\/(\d{18})$/);
-  if (!matches) return notFound.clone();
+
+  if (!matches) {
+    return new Response("No valid ID", { status: 400 });
+  }
+
   const [_, id] = matches;
+
   try {
-    const { avatar }: { avatar: string } = await manager.runMethod(
-      manager,
-      "GET",
-      `/users/${id}`
-    );
-    if (!avatar) return notFound.clone();
-    return Response.redirect(
-      `https://cdn.discordapp.com/avatars/${id}/${avatar}.png?size=2048`
-    );
+    const { avatar, discriminator }: { avatar: string; discriminator: string } =
+      await manager.runMethod(manager, "GET", `/users/${id}`);
+
+    if (avatar) {
+      return Response.redirect(
+        `https://cdn.discordapp.com/avatars/${id}/${avatar}.png?size=2048`
+      );
+    } else {
+      return Response.redirect(
+        `https://cdn.discordapp.com/embed/avatars/${
+          parseInt(discriminator) % 5
+        }.png`
+      );
+    }
   } catch {
-    return notFound.clone();
+    return new Response("User not found", { status: 404 });
   }
 });
